73472c7ce8dce9da3c3c02cc453b0a4d515b35a2
Calin Balazs <balazs_calin@yahoo.com>
2021-06-05 13:29:22 +0300

    path change

--- src/app/config/app-config.ts
+++ src/app/config/app-config.ts
@@ -1,4 +1,4 @@
 export class AppConfig {
-  public static API_PATH = 'https://aqueous-escarpment-26894.herokuapp.com';
+  public static API_PATH = 'https://sda-online-store-be.herokuapp.com';
   public static AUTHORISATION_HEADER = 'auth_header';
 }
